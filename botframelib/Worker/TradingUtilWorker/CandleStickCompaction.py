fce3ab431c198a9c76e075609a9a5853bc239f02
shoheitakaichi <shohei.takaichi@gmail.com>
2022-10-28 19:39:17 +0900

    update

--- botframelib/Worker/TradingUtilWorker/CandleStickCompaction.py
+++ botframelib/Worker/TradingUtilWorker/CandleStickCompaction.py
@@ -17,12 +17,12 @@ class CandleStickCompaction(IWorker):
             self.candleStick[event.exchange + event.symbol].low = min(event.low, self.candleStick[event.exchange + event.symbol].low )
             self.candleStick[event.exchange + event.symbol].close = event.close
             self.candleStick[event.exchange + event.symbol].volume += event.volume 
-            if self.event.time % 300 // 60 == 4:
+            if event.time % 300 // 60 == 4:
                 self.eventstory.put(
                     self.candleStick.pop(event.exchange + event.symbol)
                 )
         else:
-            if self.event.time % 300 // 60 == 0:
+            if event.time % 300 // 60 == 0:
                 self.candleStick[event.exchange + event.symbol] = CompactionCandle5min(
                     exchange=event.exchange,
                     symbol=sevent.symbol,
